ee61e14ab51910fd02d80207d8703bd3f559051f
R-HOFF <r.hoffbuhr@stud.bhh.de>
2023-06-30 16:57:57 +0200

    feat: added test endpoint

--- src/api/app.py
+++ src/api/app.py
@@ -6,6 +6,10 @@ class CalendarAPI:
         @self.app.get("/")
         def home():
             return {"Test": "Is Alive"}
+
+        @self.app.get("/test/{date}")
+        def test(date:int):
+            return {date: {("13:00", "14:00"): "Meeting"}}
     
     def start(self, port=8000, host="0.0.0.0"):
         uvicorn.run(self.app, port=port, host=host)
